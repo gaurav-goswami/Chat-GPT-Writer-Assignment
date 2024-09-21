b89ceab806d4629ed0d189968eddacfec73483a4
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2024-09-21 15:55:08 +0530

    add tailwindcss

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,35 +1,10 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
-    </>
-  );
+    <div className="w-[300px] p-6 outline-none border-none">
+      <h2 className="text-center font-semibold text-xl">LinkedIn Reply Extension</h2>
+    </div>
+  )
 }
 
-export default App;
+export default App
