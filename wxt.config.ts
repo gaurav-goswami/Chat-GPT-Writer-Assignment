d0cb247927123418fcb793bc9f3b34f9edb11abd
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2024-09-21 15:57:46 +0530

    add ai-reply-button feature

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  manifest: {
+    action: {
+      default_title: 'LinkedIn reply extension',
+    }
+  },
 });
