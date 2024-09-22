9c708d258476f6583c67fef1156ac4ac1da36618
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2024-09-23 00:01:20 +0530

    refactor code

--- components/Buttons/GenPromptBtn.tsx
+++ components/Buttons/GenPromptBtn.tsx
@@ -1,18 +1,36 @@
 import GenerateIcon from '../../assets/send-command.svg';
+import InsertPromptBtn from './InsertPromptBtn';
+import RegenerateIcon from '../../assets/regenerate.svg';
 
-const GenPromptBtn = () => {
+interface GenPromptBtnProps {
+    messages: {
+        message: string;
+        isUser: boolean;
+    }[];
+    handleGenerate: () => void;
+}
+
+const GenPromptBtn = ({ messages, handleGenerate }: GenPromptBtnProps) => {
     return (
-        <>
+
+        <div className='flex items-center gap-4'>
+            {
+                messages.length > 0 && <InsertPromptBtn />
+            }
             <button
-                className='text-white p-2 rounded-lg w-max px-6 py-2 text-md flex gap-2 items-center self-end'
+                className='text-white p-2 rounded-lg w-max px-6 py-2 text-md flex gap-2 items-center self-end h-12'
                 style={{ backgroundColor: '#3B82F6' }}
+                onClick={messages.length < 1 ? handleGenerate : undefined}
             >
-                <img src={GenerateIcon} alt="Generate" className='w-5 h-5' />
+                <img src={messages.length > 0 ? RegenerateIcon : GenerateIcon} alt="Generate" className='w-5 h-5' />
                 <span style={{ fontSize: '12px' }}>
-                    Generate
+                    {
+                        messages.length > 0 ? 'Regenerate' : 'Generate'
+                    }
                 </span>
             </button>
-        </>
+        </div>
+
     )
 }
 
